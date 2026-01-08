a0a5d359566e7bb27668adab947f184d18f192ea
Tukur Sunusi gama <gamaabba7@gmail.com>
2026-01-08 16:13:29 +0100

    correct url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/student-stage",
+  base: "/SSTG/",
   server: {
     port: 5173, // Vite default port
     host: true, // Allow external access
