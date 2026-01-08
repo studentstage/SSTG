44b84379fcf29e5fdc09bb672963fb69a340fced
Tukur Sunusi gama <gamaabba7@gmail.com>
2026-01-08 15:47:02 +0100

    diploy correction

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/student-stage",
   server: {
     port: 5173, // Vite default port
     host: true, // Allow external access
