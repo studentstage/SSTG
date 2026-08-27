86d5fc86708e33e54b86a4a20f1cd0852b37f885
Student Stage Team <studentstage001@gmail.com>
2026-08-27 15:27:28 +0100

    feat: add Student Stage team credits

---
+++ src/config/team.js
@@ -0,0 +1,12 @@
+export const team = Object.freeze({
+  name: "Student Stage Team",
+  email: "studentstage001@gmail.com",
+  members: [
+    {
+      name: "Tukur Sunusi Gama",
+      contribution: "Front-end and connections",
+    },
+    { name: "Ahamad Sani", contribution: "Back-end" },
+    { name: "Sani Auwal", contribution: "UI/UX" },
+  ],
+});
